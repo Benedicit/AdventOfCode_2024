676588cf4d489ed733216f90c3a8c755dd4b8b6b
Benedikt Weis <benedikt.georg.weis@gmail.com>
2024-12-01 18:31:44 +0100

    My solution for both parts of day 1

---
+++ Day1.py
@@ -0,0 +1,28 @@
+import numpy as np
+
+list1 = []
+list2 = []
+with open("./input/Day1.txt", "r") as f:
+    for line in f:
+        temp = line.strip().split("   ")
+        list1.append(int(temp[0]))
+        list2.append(int(temp[1]))
+def part1(list1, list2):
+    list1.sort()
+    list2.sort()
+    sum_distances = 0
+    for x, y in zip(list1, list2):
+        sum_distances += abs(x - y)
+    print("The sum of distances is", sum_distances)
+def part2(list1, list2):
+    similarity_score = 0
+    list1 = np.array(list1)
+    list2 = np.array(list2)
+    unique, count = np.unique(list2, return_counts=True)
+    counts = dict(zip(unique, count))
+    for x in list1:
+        if x in counts:
+            similarity_score += counts[x] * x
+    print("The similiarity score is", similarity_score)
+part1(list1, list2)
+part2(list1, list2)
