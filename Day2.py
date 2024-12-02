1a1be32b133172f01b089cab42360823ddb244eb
Benedikt Weis <benedikt.georg.weis@gmail.com>
2024-12-02 13:58:33 +0100

    start of a small clean-up

--- Day2.py
+++ Day2.py
@@ -1,4 +1,29 @@
-import numpy as np
+def check_safe(numbers):
+    prev = int(numbers[0])
+    ascending = 0
+    safe = True
+    for i in range(1, len(numbers)):
+        x = int(numbers[i])
+        if x == prev:
+            safe = False
+            break
+        elif x < prev:
+            if ascending == 1:
+                safe = False
+                break
+            elif ascending == 0:
+                ascending = -1
+        elif x > prev:
+            if ascending == -1:
+                safe = False
+                break
+            elif ascending == 0:
+                ascending = 1
+        if abs(x -prev) > 3:
+            safe = False
+            break
+        prev = x
+    return safe
 
 
 def part1():
@@ -7,11 +32,28 @@ def part1():
         unsafe_reports = []
         for line in f:
             numbers = line.split(" ")
-            prev = int(numbers[0])
+            safe = check_safe(numbers)
+            if safe:
+                safe_reports += 1
+            else:
+                unsafe_reports.append(numbers)
+        print(safe_reports)
+        return safe_reports, unsafe_reports
+
+def part2():
+    safe_reports, unsafe_reports = part1()
+    for report in unsafe_reports:
+        for skip in range(0, len(report)):
+            prev = int(report[0])
             ascending = 0
             safe = True
-            for i in range(1, len(numbers)):
-                x = int(numbers[i])
+            for i in range(1, len(report)):
+                if i == skip:
+                    continue
+                elif (skip == 0 and i == 1) :
+                    prev = int(report[i])
+                    continue
+                x = int(report[i])
                 if x == prev:
                     safe = False
                     break
@@ -36,49 +78,6 @@ def part1():
                 prev = x
             if safe:
                 safe_reports += 1
-            else:
-                unsafe_reports.append(numbers)
-        print(safe_reports)
-        return safe_reports, unsafe_reports
-def part2():
-    with open("./input/Day2.txt") as f:
-        safe_reports, unsafe_reports = part1()
-        for report in unsafe_reports:
-            for skip in range(0, len(report)):
-                prev = int(report[0])
-                ascending = 0
-                safe = True
-                for i in range(1, len(report)):
-                    if i == skip:
-                        continue
-                    elif (skip == 0 and i == 1) :
-                        prev = int(report[i])
-                        continue
-                    x = int(report[i])
-                    if x == prev:
-                        safe = False
-                        break
-                    if x < prev:
-                        if ascending == 1:
-                            safe = False
-                            break
-                        elif ascending == 0:
-                            ascending = -1
-                        if x < prev - 3:
-                            safe = False
-                            break
-                    elif x > prev:
-                        if ascending == -1:
-                            safe = False
-                            break
-                        elif ascending == 0:
-                            ascending = 1
-                        if x > prev + 3:
-                            safe = False
-                            break
-                    prev = x
-                if safe:
-                    safe_reports += 1
-                    break
-        print(safe_reports)
+                break
+    print(safe_reports)
 part2()
