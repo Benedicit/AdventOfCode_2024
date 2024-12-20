5eed736ca5db7469a6eb978f331edd0c9e92693f
Benedikt Weis <benedikt.georg.weis@gmail.com>
2024-12-20 15:16:45 +0100

    Solution for day 19 part 1 and part 2

---
+++ Day19.py
@@ -0,0 +1,60 @@
+import functools
+import re
+
+patterns = []
+
+@functools.cache
+def combs(s):
+    """
+    :param s: current string
+    :return: number of possible combinations
+    """
+    result = 0
+    if s == "":
+        return 1
+    else:
+        for p in patterns:
+            if s.startswith(p):
+                result += combs(s[len(p):])
+        return result
+
+with open("input/Day19.txt") as f:
+    towels = ""
+    targets = []
+    skip = False
+    valid_t = []
+    for line in f:
+        line = line.strip()
+        if skip:
+            targets.append(line)
+        elif line == "":
+            skip = True
+            continue
+        else:
+            towels += line
+    temp = towels.split(", ")
+    regex = ""
+    for t in temp:
+        regex += f"({t})?"
+    regex = f"^({regex})*$"
+    sum_combs = 0
+    valid_towels = 0
+    for t in targets:
+        search = re.findall(regex, t)
+        if search:
+            valid_t.append(t)
+            valid_towels += 1
+    print(f"Part 1: {valid_towels}")
+    patterns = temp
+    for t in valid_t:
+        sum_combs += combs(t)
+    print(f"Part 2: {sum_combs}")
+
+
+
+
+
+
+
+
+
