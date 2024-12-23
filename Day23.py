dafcf51b3e1f203b972897a11a0b63c993204004
Benedikt Weis <benedikt.georg.weis@gmail.com>
2024-12-23 12:02:39 +0100

    Added some comments

--- Day23.py
+++ Day23.py
@@ -1,5 +1,6 @@
 from itertools import combinations, chain
 
+# Build the graph
 nodes = {}
 with open("input/Day23.txt") as f:
     for line in f:
@@ -14,46 +15,56 @@ with open("input/Day23.txt") as f:
             nodes[c2].add(c1)
 
 def part1():
-        temp = set()
-        for c in nodes:
-            if not c.startswith("t"):
-                continue
-            current = nodes[c]
-            for a,b in combinations(current, 2):
-                A = nodes[a]
-                B = nodes[b]
-                x = [a, b, c]
-                x.sort()
-                x = tuple(x)
-                if b in A and a in B and x not in temp:
-                    temp.add(x)
-        print(len(temp))
-        fully_connected = []
-        for n in nodes:
-            nodes[n].add(n)
-        for c in nodes:
-            current = nodes[c]
-            full = current
-            full.add(c)
-            subset = set()
-            for x in powerset(current):
-                p = set(x)
-                every = True
-                for z in list(x):
-                    if p - nodes[z] != set():
-                        every = False
-                        break
-                if every and len(p)> len(subset):
-                    subset = p
-            current.add(c)
-            if len(subset) > len(fully_connected):
-                fully_connected = subset
-        fully_connected = list(fully_connected)
-        fully_connected.sort()
-        result = ",".join(fully_connected)
-        print(result)
+    """
+    only take nodes which start with "t" and check if both neighbors are connected. To not have duplicates
+    there is set which takes all sorted tuples (there are sadly not sets of sets).
+    """
+    temp = set()
+    for c in nodes:
+        if not c.startswith("t"):
+            continue
+        current = nodes[c]
+        for a,b in combinations(current, 2):
+            A = nodes[a]
+            B = nodes[b]
+            x = [a, b, c]
+            x.sort()
+            x = tuple(x)
+            if b in A and a in B and x not in temp:
+                temp.add(x)
+    print("Part 1:", len(temp))
+def part2():
+    """
+    For the biggest clique, for every node take the biggest subset of neighbors which are fully connected
+    """
+    fully_connected = []
+    for n in nodes:
+        nodes[n].add(n) # The node itself is needed such the set operations work correct
+    for c in nodes:
+        current = nodes[c]
+        subset = set()
+        for x in powerset(current):
+            p = set(x)
+            every = True
+            for z in list(x):
+                if p - nodes[z] != set():
+                    every = False
+                    break
+            if every and len(p)> len(subset):
+                subset = p
+        current.add(c)
+        if len(subset) > len(fully_connected):
+            fully_connected = subset
+    fully_connected = list(fully_connected)
+    fully_connected.sort()
+    result = ",".join(fully_connected)
+    print("Part 2:",result)
 
 def powerset(iterable):
+    """
+    Helper function to generate to powerset
+    """
     s = list(iterable)
     return chain.from_iterable(combinations(s, r) for r in range(1, len(s)+1))
 part1()
+part2()
